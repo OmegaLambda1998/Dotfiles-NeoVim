3b78669923f5c62d3fd02421c40bc3eee52ce9d3
Patrick Armstrong <patrick.james.1998@gmail.com>
2024-11-19 12:51:19 +1100

    fzf

--- lua/OmegaLambda/editor/fzf.lua
+++ lua/OmegaLambda/editor/fzf.lua
@@ -2,3 +2,36 @@ local spec, opts = OL.spec:add("ibhagwan/fzf-lua")
 spec.cmd = {
     "FzfLua",
 }
+
+spec.keys = {
+    {
+        "<leader>zf",
+        function()
+            vim.cmd("FzfLua files")
+        end,
+        desc = "Open Files",
+    },
+    {
+        "<leader>zh",
+        function()
+            vim.cmd("FzfLua oldfiles")
+        end,
+        desc = "Open Recent Files",
+    },
+    {
+        "<leader>zg",
+        function()
+            vim.cmd("FzfLua live_grep_native")
+        end,
+        desc = "Grep Project",
+    },
+}
+
+OL.map(
+  {
+      "<leader>z",
+      group = "FzF",
+      desc = "FzF",
+      { spec.keys },
+  }
+)
